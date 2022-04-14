6a4151643614727776ead44f7c3a1e5f00823322
Cody Elhard <32208000+cody-elhard@users.noreply.github.com>
2022-04-14 11:04:18 -0500

    make CRA communicate with rails app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://localhost:3000/posts.json');
+      console.log('response', response);
+      const body = await response.json();
+      setPosts(body);
+    }
+
+    fetchData()
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> My Posts 2 </h1>
+
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            {post.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
